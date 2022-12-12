6b089b2b594a3d1bb0b64736f9687cc2352a8554
Lum Beqiri <lumbeqiri@live.com>
2022-12-12 19:41:59 +0100

    remove from cart test

--- tests/Feature/Buyer/BuyerCartControllerTest.php
+++ tests/Feature/Buyer/BuyerCartControllerTest.php
@@ -11,11 +11,11 @@ it('can add an item to the cart', function(){
     $seller = User::factory()->create();
     $buyer = User::factory()->create();
 
-    login($buyer);
-
     $product = Product::factory()->create(['seller_id' => $seller->id]);
-    $variant = Variant::factory()->available()->for($product)->create();
+    $variant = Variant::factory()->available()->for($product)->create(['stock' => 50]);
     $count = 2;
+
+    login($buyer);
 
     $response = $this->postJson(action([BuyerCartController::class, 'add_to_cart']), [
         'variant_id' => $variant->uuid,
@@ -25,5 +25,33 @@ it('can add an item to the cart', function(){
     $response->assertOk();
 
     $this->assertDatabaseHas(CartItem::class, ['variant_id' => $variant->id, 'count' => $count, 'cart_id' => $buyer->cart->id]);
+    $this->assertDatabaseHas(Cart::class, ['user_id' => $buyer->id]);
+});
+
+it('can remove an item to the cart', function(){
+    $seller = User::factory()->create();
+    $buyer = User::factory()->create();
+
+    $inCart = 20;
+    $toRemove = 10;
+    $itemsLeft = $inCart - $toRemove;
+
+    $product = Product::factory()->create(['seller_id' => $seller->id]);
+    $variant = Variant::factory()->available()->for($product)->create(['stock' => 50]);
+
+    login($buyer);
+
+    $cart = Cart::factory()->for($buyer)->create();
+    CartItem::factory()->for($cart)->create(['variant_id' => $variant->id, 'count' => $inCart]);
+
+
+    $response = $this->deleteJson(action([BuyerCartController::class, 'remove_from_cart']), [
+        'variant_id' => $variant->uuid,
+        'count' => $toRemove
+    ]);
+
+    $response->assertOk();
+
+    $this->assertDatabaseHas(CartItem::class, ['variant_id' => $variant->id, 'count' => $itemsLeft, 'cart_id' => $buyer->cart->id]);
     $this->assertDatabaseHas(Cart::class, ['user_id' => $buyer->id]);
 });
