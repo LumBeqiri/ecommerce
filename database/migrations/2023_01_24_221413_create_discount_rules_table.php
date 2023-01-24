ec3374cb28f212bc45d23788c2d50c65bfc2cd44
Lum Beqiri <lumbeqiri@live.com>
2023-01-24 23:24:20 +0100

    discounts wip

---
+++ database/migrations/2023_01_24_221413_create_discount_rules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discount_rules', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('description');
+            $table->enum('type', ['fixed', 'percentage', 'free_shipping']);
+            $table->double('value', 8, 2);
+            $table->enum('allocation', ['total_ammount', 'item_specific']);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discount_rules');
+    }
+};
