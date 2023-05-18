8b07bbde97eba0558514cf33eebccf68c09a6fc5
Lum Beqiri <lumbeqiri@live.com>
2023-05-18 21:03:33 +0200

    apply discount test and some refactoring

--- app/Services/DiscountService.php
+++ app/Services/DiscountService.php
@@ -20,7 +20,7 @@ class DiscountService
             return response()->json(['error' => 'Discount is not applicable', 'code' => 422], 422);
         }
 
-        $variant_discount = [];
+        // $variant_discount = [];
 
         $discount = Discount::with('discount_rule')->where('code', $discount_code)->firstOrFail();
         $discount_rule = $discount->discount_rule;
@@ -35,75 +35,77 @@ class DiscountService
         }
 
         // it discounts the whole cart total price with a fixed amount
-        self::calculate_whole_cart_discount($discount_rule, $cart, $discount_region);
-
-        self::calculate_percentage_cart_discount($discount_rule, $cart);
-
-        $variants = Variant::with('product.discount_conditions')
-        ->whereIn('id', $cart->cart_items->pluck('variant_id'))
-        ->get();
-
-        foreach ($variants as $variant) {
-            $product = $variant->product;
-
-            $discount_conditions = $product->discount_conditions()->where('discount_rule_id', $discount_rule->id)->get(['operator']);
-
-            foreach ($discount_conditions as $discount_condition) {
-                if ($discount_condition->operator === DiscountConditionOperatorTypes::NOT_IN) {
-                    continue;
-                }
-                $discount_value = $discount_rule->value;
-                $discount_type = $discount_rule->discount_type;
-
-                $temp = [
-                    'variant' => $variant->uuid,
-                    'value' => $discount_value,
-                    'type' => $discount_type,
-                ];
-
-                $variant_discount[] = $temp;
-            }
+        if ($discount_rule->discount_type === DiscountRuleTypes::FIXED_AMOUNT && $discount_rule->allocation === DiscountAllocationTypes::TOTAL_AMOUNT) {
+            return self::calculate_whole_cart_discount($discount_rule, $cart, $discount_region);
         }
 
-        return $variant_discount;
+        if ($discount_rule->discount_type === DiscountRuleTypes::PERCENTAGE && $discount_rule->allocation === DiscountAllocationTypes::TOTAL_AMOUNT) {
+            self::calculate_percentage_cart_discount($discount_rule, $cart);
+        }
+        // $variants = Variant::with('product.discount_conditions')
+        // ->whereIn('id', $cart->cart_items->pluck('variant_id'))
+        // ->get();
+
+        // if($discount_rule->discount_conditions()->exists()){
+
+        // }
+        // foreach ($variants as $variant) {
+        //     $product = $variant->product;
+
+        //     $discount_conditions = $product->discount_conditions()->where('discount_rule_id', $discount_rule->id)->get(['operator']);
+
+        //     foreach ($discount_conditions as $discount_condition) {
+        //         if ($discount_condition->operator === DiscountConditionOperatorTypes::NOT_IN) {
+        //             continue;
+        //         }
+        //         $discount_value = $discount_rule->value;
+        //         $discount_type = $discount_rule->discount_type;
+
+        //         $temp = [
+        //             'variant' => $variant->uuid,
+        //             'value' => $discount_value,
+        //             'type' => $discount_type,
+        //         ];
+
+        //         $variant_discount[] = $temp;
+        //     }
+        // }
+
+        // return $variant_discount;
     }
 
     private static function calculate_whole_cart_discount(DiscountRule $discount_rule, Cart $cart, Region $discount_region)
     {
-        if ($discount_rule->discount_type === DiscountRuleTypes::FIXED_AMOUNT && $discount_rule->allocation === DiscountAllocationTypes::TOTAL_AMOUNT) {
-            $value = 0;
-            if ($cart->region->id !== $discount_region->id) {
-                return response()->json(['error' => 'Discount is not applicable', 'code' => 422], 422);
-            }
-            if ($discount_region->currency->has_cents) {
-                $value = $discount_rule->value * 100;
-            }
-
-            $value = $discount_rule->value;
-
-            $cart->total_cart_price -= (int) $value;
-
-            if ($cart->total_cart_price < 0) {
-                $cart->total_cart_price = 0;
-            }
-            $cart->has_been_discounted = true;
-            $cart->save();
-
-            return response()->json(['message' => 'Discount applied successfully', 'code' => 200], 200);
+        $value = 0;
+        if ($cart->region->id !== $discount_region->id) {
+            return response()->json(['error' => 'Discount is not applicable', 'code' => 422], 422);
         }
+        if ($discount_region->currency->has_cents) {
+            $value = $discount_rule->value * 100;
+        }
+
+        $value = $discount_rule->value;
+
+        $cart->total_cart_price -= (int) $value;
+
+        if ($cart->total_cart_price < 0) {
+            $cart->total_cart_price = 0;
+        }
+        $cart->has_been_discounted = true;
+        $cart->save();
+
+        return response()->json(['message' => 'Discount applied successfully', 'code' => 200], 200);
     }
 
     private static function calculate_percentage_cart_discount(DiscountRule $discount_rule, Cart $cart)
     {
-        if ($discount_rule->discount_type === DiscountRuleTypes::PERCENTAGE && $discount_rule->allocation === DiscountAllocationTypes::TOTAL_AMOUNT) {
-            $cart->total_cart_price = $cart->total_cart_price - ($cart->total_cart_price * ($discount_rule->value / 100));
-            if ($cart->total_cart_price < 0) {
-                $cart->total_cart_price = 0;
-            }
-            $cart->has_been_discounted = true;
-            $cart->save();
-
-            return response()->json(['message' => 'Discount applied successfully', 'code' => 200], 200);
+        $cart->total_cart_price = $cart->total_cart_price - ($cart->total_cart_price * ($discount_rule->value / 100));
+        if ($cart->total_cart_price < 0) {
+            $cart->total_cart_price = 0;
         }
+        $cart->has_been_discounted = true;
+        $cart->save();
+
+        return response()->json(['message' => 'Discount applied successfully', 'code' => 200], 200);
     }
 }
--- tests/Feature/Buyer/BuyerCartControllerTest.php
+++ tests/Feature/Buyer/BuyerCartControllerTest.php
@@ -5,12 +5,16 @@ use App\Models\Cart;
 use App\Models\CartItem;
 use App\Models\Country;
 use App\Models\Currency;
+use App\Models\Discount;
+use App\Models\DiscountRule;
 use App\Models\Product;
 use App\Models\Region;
 use App\Models\TaxProvider;
 use App\Models\User;
 use App\Models\Variant;
 use App\Models\VariantPrice;
+use App\values\DiscountAllocationTypes;
+use App\values\DiscountRuleTypes;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Notification;
 
@@ -84,4 +88,64 @@ it('can remove an item to the cart', function () {
 
     $this->assertDatabaseHas(CartItem::class, ['variant_id' => $variant->id, 'quantity' => $itemsLeft, 'cart_id' => $buyer->cart->id]);
     $this->assertDatabaseHas(Cart::class, ['user_id' => $buyer->id]);
+});
+
+it('can apply discount', function () {
+    Currency::factory()->create();
+    TaxProvider::factory()->create();
+    $region = Region::factory()->create();
+    $country = Country::factory()->for($region)->create();
+
+    $buyer = User::factory()->create(['country_id' => $country->id]);
+
+    $product = Product::factory()->available()->create();
+    $variant1 = Variant::factory()->available()->for($product)->create(['stock' => 30]);
+    $variant2 = Variant::factory()->available()->for($product)->create(['stock' => 30]);
+
+    VariantPrice::factory()->create([
+        'variant_id' => $variant1->id,
+        'region_id' => $region->id,
+        'price' => 100,
+    ]);
+
+    VariantPrice::factory()->create([
+        'variant_id' => $variant2->id,
+        'region_id' => $region->id,
+        'price' => 200,
+    ]);
+
+    $cart = Cart::factory()->create(['user_id' => $buyer->id, 'total_cart_price' => 300, 'has_been_discounted' => false, 'region_id' => $region->id]);
+
+    CartItem::factory()->create([
+        'cart_id' => $cart->id,
+        'variant_id' => $variant1->id,
+    ]);
+
+    CartItem::factory()->create([
+        'cart_id' => $cart->id,
+        'variant_id' => $variant2->id,
+    ]);
+
+    $discount_rule = DiscountRule::factory()->create([
+        'region_id' => $region->id,
+        'discount_type' => DiscountRuleTypes::FIXED_AMOUNT,
+        'value' => 100,
+        'allocation' => DiscountAllocationTypes::TOTAL_AMOUNT,
+    ]);
+
+    Discount::factory()->create([
+        'code' => 'lum',
+        'is_disabled' => false,
+        'discount_rule_id' => $discount_rule->id,
+    ]);
+
+    login($buyer);
+
+    $response = $this->postJson(action([BuyerCartController::class, 'apply_discount']), [
+        'code' => 'lum',
+    ]);
+
+    $response->assertOk();
+
+    $this->assertDatabaseHas(Cart::class, ['total_cart_price' => 200]);
 });
