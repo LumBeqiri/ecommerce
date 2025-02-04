b3d3364714af0c0f1f2d6512393071b82dbb6e72
Lum Beqiri <lumbeqiri@live.com>
2025-02-04 20:40:09 +0100

    product update and policy update

--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -16,19 +16,19 @@ class ProductPolicy
      *
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    // public function view(User $user, Product $product)
-    // {
-    //     if ($user->hasRole('admin')) {
-    //         return Response::allow();
-    //     }
-    //     if ($user->hasRole('vendor') && $user->vendor->ownsProduct($product)) {
-    //         return Response::allow();
-    //     }
+    public function view(User $user, Product $product)
+    {
+        if ($user->hasRole('admin')) {
+            return Response::allow();
+        }
+        if ($user->hasRole('vendor') && $user->vendor->ownsProduct($product)) {
+            return Response::allow();
+        }
 
-    //     return $user->hasPermissionTo('manage-products') && $user->staff->vendor_id == $product->vendor->user_id
-    //     ? Response::allow()
-    //     : Response::deny('You do not own this product.');
-    // }
+        return $user->hasPermissionTo('manage-products') && $user->staff->vendor_id == $product->vendor->user_id
+        ? Response::allow()
+        : Response::deny('You do not own this product.');
+    }
 
     /**
      * Determine whether the user can create models.
