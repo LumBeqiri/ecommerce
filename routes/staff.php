17fa8786158216b2dc4e2da1079e3a8c431c641c
Lum Beqiri <lumbeqiri@live.com>
2025-02-05 22:59:27 +0100

    cleanup

--- routes/staff.php
+++ routes/staff.php
@@ -8,9 +8,4 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
     Route::get('variants/{variant}/attributes', [StaffVariantAttributeController::class, 'index']);
     Route::put('variants/{variant}/attributes', [StaffVariantAttributeController::class, 'update']);
-
-    Route::post('variants/{variant}/prices', [StaffVariantPriceController::class, 'store']);
-    Route::put('variants/{variant}/prices/{variantPrice}', [StaffVariantPriceController::class, 'update']);
-    Route::delete('variants/{variant}/prices/{variantPrice}', [StaffVariantPriceController::class, 'destroy']);
-
 });
