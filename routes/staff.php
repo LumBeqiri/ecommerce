6d0bb10fd9deabca3618f2928819a2a29eaa843d
Lum Beqiri <lumbeqiri@live.com>
2025-02-05 22:59:42 +0100

    pint

--- routes/staff.php
+++ routes/staff.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Staff\StaffVariantAttributeController;
-use App\Http\Controllers\Staff\StaffVariantPriceController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
