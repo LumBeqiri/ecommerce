4ca9cc6c1df461847b7283c180c5c239c40c15d3
Lum Beqiri <lumbeqiri@live.com>
2025-02-06 12:13:56 +0100

    variant media management from userController

---
+++ app/Http/Controllers/User/Variants/UserVariantMediaController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\User\Variants;
+
+
+use App\Http\Controllers\ApiController;
+use App\Http\Requests\Media\MediasRequest;
+use App\Http\Resources\MediaResource;
+use App\Http\Resources\VariantResource;
+use App\Models\Variant;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class UserVariantMediaController extends ApiController
+{
+    public function index(Variant $variant): JsonResponse
+    {
+        $this->authorize('view',$variant);
+        $medias = $variant->medias;
+
+        return $this->showAll(MediaResource::collection($medias));
+    }
+
+    public function store(MediasRequest $request, Variant $variant): JsonResponse
+    {
+        $this->authorize('update',$variant);
+
+        $medias = $request->file('files');
+
+        try {
+            foreach ($medias as $media) {
+                $variant->addMedia($media)
+                    ->toMediaCollection('variants');
+            }
+        } catch (Exception $ex) {
+            return $this->showError($ex->getMessage());
+        }
+
+        return $this->showOne(new VariantResource($variant));
+    }
+
+    public function destroy(Variant $variant, $media_uuid): JsonResponse
+    {
+        $this->authorize('delete',$variant);
+        
+        $media = Media::where('uuid', $media_uuid)->firstOrFail();
+        if ($variant->id !== $media->model_id) {
+            return $this->showError('This media does not belong to the specified variant.');
+        }
+
+        try {
+            $media->delete();
+        } catch (Exception $ex) {
+            return $this->showError($ex->getMessage(), $ex->getCode());
+        }
+
+        return $this->showMessage('Image deleted successfully!');
+    }
+}
--- routes/vendor.php
+++ routes/vendor.php
@@ -7,10 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
-    Route::get('variants/{variant}/medias', [AdminVariantMediaController::class, 'index']);
-    Route::post('variants/{variant}/medias', [AdminVariantMediaController::class, 'store']);
-    Route::delete('variants/{variant}/medias/{media_id}', [AdminVariantMediaController::class, 'destroy']);
-
+   
     Route::get('user-permissions', [VendorPermissionManagerController::class, 'index']);
     Route::put('users/{user}/permissions', [VendorPermissionManagerController::class, 'update']);
     Route::delete('users/{user}/permissions/{permission_id}', [VendorPermissionManagerController::class, 'destroy']);
