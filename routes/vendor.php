e73328f65dea3ec645e6f88fef39624e6aec5885
Lum Beqiri <lumbeqiri@live.com>
2025-02-06 12:06:46 +0100

    pint

--- routes/vendor.php
+++ routes/vendor.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Admin\Product\AdminVariantMediaController;
 use App\Http\Controllers\Auth\Staff\RegisterStaffController;
-use App\Http\Controllers\Product\ProductThumbnailController;
 use App\Http\Controllers\Vendor\VendorPermissionManagerController;
 use Illuminate\Support\Facades\Route;
 
