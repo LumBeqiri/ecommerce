4af136732ffc1eb46a1ea34f26fc3215b635ec4d
Lum Beqiri <lumbeqiri@live.com>
2025-02-13 13:17:54 +0100

    register will not sync the cart

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -19,20 +19,7 @@ class RegisterController extends ApiController
 
         $token = $user->createToken('ecommerceToken')->plainTextToken;
 
-        if ($request->hasCookie('cart')) {
-            $cookie_cart = $request->cookie('cart');
-
-            if (is_string($cookie_cart)) { // Check if $cookie_cart is a string
-                $items = json_decode($cookie_cart, true);
-
-                if (! empty($items) && is_array($items) && array_key_exists('items', $items)) {
-                    $items = $items['items'];
-                    // TODO: Sync items from cookie to cart
-                }
-            }
-        }
-
-        $response = [
+        $response = [   
             'user' => $user,
             'token' => $token,
         ];
