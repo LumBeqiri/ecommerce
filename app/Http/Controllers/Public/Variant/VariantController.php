767eb0cc261f569173e55dd9cfe1529d5dd5c4cc
Lum Beqiri <lumbeqiri@live.com>
2025-02-26 10:25:13 +0100

    larastan fixes

--- app/Http/Controllers/Public/Variant/VariantController.php
+++ app/Http/Controllers/Public/Variant/VariantController.php
@@ -7,22 +7,13 @@ use App\Http\Resources\VariantResource;
 use App\Models\Country;
 use App\Models\Variant;
 use Illuminate\Http\JsonResponse;
-use Stevebauman\Location\Facades\Location;
 
 class VariantController extends ApiController
 {
     public function index(): JsonResponse
     {
-        $ip = '185.190.132.204';
-        $country_name = '';
-
-        $position = Location::get($ip);
-        if ($position instanceof \Stevebauman\Location\Position) {
-            $country_name = $position->countryName;
-        }
-
         $region_id = Country::select('region_id')
-            ->where('name', 'LIKE', '%'.$country_name.'%')
+            ->where('code', 'DE')
             ->value('region_id');
 
         $variants = Variant::with(['media', 'variant_prices' => function ($query) use ($region_id) {
